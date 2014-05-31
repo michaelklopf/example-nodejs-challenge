59dece6d546752433df4d0d00bda2d3d86d31c2a
Michael Klopf <klopf.michael@gmail.com>
2014-05-31 21:15:37 +0200

    Challenge completed.

--- source/static/js/core.js
+++ source/static/js/core.js
@@ -1,10 +1,38 @@
 $(document).ready(function() {
   $.getJSON("/commits", function(data) {
+    //console.log(data);
     for (var i = 0; i < data.length; i++) {
       var author = '<div>' + data[i].commit.author.name + '</div><br>';
       $('#content').append(author);
-      var commit = '<div>' + data[i].commit.tree.sha + '</div><br>';
-      $('#content').append(commit);
+
+      var sha = new String(data[i].commit.tree.sha);
+      addCommitShaToContent(sha);
     }
   });
 });
+
+var addCommitShaToContent = function(sha) {
+  $('#content').append(getContentWithColorClass(sha));
+};
+
+var getContentWithColorClass = function(sha) {
+  var colorClass = getCorrectColorClassForSha(sha);
+  var commit = '<div class="' + colorClass + '">' + sha + '</div><br>';
+  return commit;
+};
+
+var getCorrectColorClassForSha = function(sha) {
+  if (lastCharIsNumber(sha)) {
+    return 'lightBlue';
+  } else {
+    return 'darkBlue';
+  }
+};
+
+var lastCharIsNumber = function(sha) {
+  if (isNaN(parseInt(sha[sha.length-1]))) {
+    return false;
+  } else {
+    return true;
+  }
+};
